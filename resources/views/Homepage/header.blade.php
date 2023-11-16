a6b9f0fc9eddf43915e035981c410a58b9521479
rafin8990 <rafin8990@gmail.com>
2023-11-16 16:35:47 +0600

    HEADER  [done]

---
+++ resources/views/Homepage/header.blade.php
@@ -0,0 +1,47 @@
+<div
+    class="relative overflow-hidden bg-cover bg-no-repeat"
+    style="
+      background-position: 50%;
+      background-image: url('https://cdn.britannica.com/35/230835-050-3D93A72A/School-children-wearing-school-uniforms.jpg');
+      height: 600px;
+    ">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+      style="background-color: rgba(0, 0, 0, 0.75)">
+      <div class="flex h-full items-center justify-center">
+        <div class="px-6 text-center text-white md:px-12">
+          <h1 class="mb-6  sm:text-5xl lg:text-[80px] font-bold text-accent ">School Management </h1>
+          <h3 class="mb-8 sm:text-xl lg:text-3xl font-bold sm:w-[300px] lg:w-[600px] mx-auto ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius accusamus tempora necessitatibus dicta provident sunt quidem quod.</h3>
+          <button
+            type="button"
+            class="btn btn-accent  text-white ">
+            Get started
+          </button>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="lg:absolute bottom-[-10px] left-[100px]">
+            <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 lg:w-[1440px] mx-auto"> 
+    
+            <div class="p-10 border bg-[#f6a84b] rounded-xl text-white mx-2 mt-2">
+                <h1 class="text-2xl font-semibold text-center">Something</h1>
+                <p class="text-xl text-center mt-3">21 Awesome Things About Music at Smart</p>
+            </div>
+            <div class="p-10 border bg-[#71b4e1] rounded-xl text-white mx-2">
+                <h1 class="text-2xl font-semibold text-center">Something</h1>
+                <p class="text-xl text-center mt-3">21 Awesome Things About Music at Smart</p>
+            </div>
+            <div class="p-10 border bg-[#bcbec0] rounded-xl text-white mx-2">
+                <h1 class="text-2xl font-semibold text-center">Something</h1>
+                <p class="text-xl text-center mt-3">21 Awesome Things About Music at Smart</p>
+            </div>
+            <div class="p-10 border bg-[#66bd6d] rounded-xl text-white mx-2">
+                <h1 class="text-2xl font-semibold text-center">Something</h1>
+                <p class="text-xl text-center mt-3">21 Awesome Things About Music at Smart</p>
+            </div>
+
+        </div>
+    </div>
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="light">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.0.8/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.js"></script>
 
 </head>
 <body>
     @include('/shared/navbar')
+    @include('/Homepage/header')
 </body>
 </html>
