b2593c214eea674419630aeac7e6f67d5226d969
rafin8990 <rafin8990@gmail.com>
2023-11-16 11:50:20 +0600

    navbar added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.0.8/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
-    <h1> ASma AKter</h1>
+    @include('/shared/navbar')
 </body>
 </html>
