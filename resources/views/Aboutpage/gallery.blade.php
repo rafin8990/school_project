027fad5753d9e59684aa82ed9c6a7bc038ced04f
AsmaCSE <asmaakter2331@gmail.com>
2023-11-20 12:33:52 +0600

    update about page

---
+++ resources/views/Aboutpage/gallery.blade.php
@@ -0,0 +1,55 @@
+<div class="bg-slate-600">
+    
+    <div class="md:mx-52">
+        <h2 class="text-accent text-4xl  pt-14">Gallery</h2><br><br>
+        <div class="md:flex">
+            <div class="h-96 carousel carousel-vertical mr-5">
+                <div class="carousel-item h-full ">
+                  <img src="https://www.shahidcadet-academy.com/wp-content/uploads/2018/10/6.jpg" class="w-[300px] h-[300px]">
+                </div> 
+                <div class="carousel-item h-full">
+                  <img src="https://bkspps.edu.bd/media/album/2.jpg" class="w-[300px] h-[300px]">
+                </div> 
+                <div class="carousel-item h-full">
+                  <img src="https://www.thepillarspublicschool.in/images/gallery/2131108444.jpeg" class="w-[300px] h-[300px]">
+                </div> 
+            </div>
+
+            <div class="h-96 carousel carousel-vertical mr-5 ">
+              <div class="carousel-item h-full">
+                <img src="https://www.shahidcadet-academy.com/wp-content/uploads/2018/10/6.jpg" class="w-[300px] h-[300px]">
+              </div> 
+              <div class="carousel-item h-full">
+                <img src="https://bkspps.edu.bd/media/album/2.jpg" class="w-[300px] h-[300px]">
+              </div> 
+              <div class="carousel-item h-full">
+                <img src="https://www.thepillarspublicschool.in/images/gallery/2131108444.jpeg" class="w-[300px] h-[300px]">
+              </div> 
+          </div>
+
+          <div class="h-96 carousel carousel-vertical mr-5 ">
+            <div class="carousel-item h-full">
+              <img src="https://www.shahidcadet-academy.com/wp-content/uploads/2018/10/6.jpg" class="w-[300px] h-[300px]">
+            </div> 
+            <div class="carousel-item h-full">
+              <img src="https://bkspps.edu.bd/media/album/2.jpg" class="w-[300px] h-[300px]">
+            </div> 
+            <div class="carousel-item h-full">
+              <img src="https://www.thepillarspublicschool.in/images/gallery/2131108444.jpeg" class="w-[300px] h-[300px]">
+            </div> 
+        </div>
+
+        <div class="h-96 carousel carousel-vertical ">
+          <div class="carousel-item h-full">
+            <img src="https://www.shahidcadet-academy.com/wp-content/uploads/2018/10/6.jpg" class="w-[300px] h-[300px]">
+          </div> 
+          <div class="carousel-item h-full">
+            <img src="https://bkspps.edu.bd/media/album/2.jpg" class="w-[300px] h-[300px]">
+          </div> 
+          <div class="carousel-item h-full">
+            <img src="https://www.thepillarspublicschool.in/images/gallery/2131108444.jpeg" class="w-[300px] h-[300px]">
+          </div> 
+      </div>
+        </div>
+    </div>
+</div>
