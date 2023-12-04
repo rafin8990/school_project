2c2a40b712a7bd32308a1e122dd98e409017d4a4
rafin8990 <rafin8990@gmail.com>
2023-12-04 18:39:01 +0600

    notice addded

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Notice</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.0.8/dist/full.min.css" rel="stylesheet" type="text/css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.1.1/flowbite.min.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
