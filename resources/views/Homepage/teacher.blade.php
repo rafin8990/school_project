58c0079879483cd7087969e7e8d3a600d6b2cb23
rafin8990 <rafin8990@gmail.com>
2023-12-13 11:59:23 +0600

    teacher section added

---
+++ resources/views/Homepage/teacher.blade.php
@@ -0,0 +1,59 @@
+<div class="md:flex">
+    <div class="mx-10">
+        <div class="max-w-screen-md mx-auto my-5 border border-blue-400 p-3">
+            <h1 class="text-4xl font-semibold text-center font my-10">Welcome to our school</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi quisquam perspiciatis voluptatum
+                corrupti.
+                Voluptatum ad itaque iusto? Aut laboriosam neque laudantium repellendus ab ex quos porro quae velit
+                ducimus
+                quidem consequatur aliquid nobis, nam fugiat doloremque quod esse alias error, exercitationem reiciendis
+                placeat
+                doloribus soluta. Officiis tempore iure cumque obcaecati. Ducimus asperiores tempore sequi repudiandae
+                ipsam
+                soluta saepe magni iure!</p>
+        </div>
+        <div class="max-w-screen-md mx-auto my-10 border border-blue-400">
+            <h1 class="text-4xl font-semibold font bg-blue-400 text-white p-2">Head Teacher</h1>
+            <div class="flex p-2">
+                <img class="w-1/5" src="https://selvamarts.edu.in/wp-content/uploads/2016/01/PrincipalPhoto.jpg" alt="">
+                <div class="mx-2">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi consequatur molestiae veritatis
+                        repudiandae
+                        at ab a earum reprehenderit architecto odit, dolor corrupti vel sed ratione aliquid voluptates
+                        quod
+                        fugiat unde. Dolorem non vel eligendi laudantium maiores consectetur culpa rerum delectus
+                        repudiandae
+                        reiciendis eum praesentium eaque alias placeat inventore voluptatum sint illo id voluptate
+                        necessitatibus excepturi sed, est obcaecati. Possimus non laudantium quaerat vel rerum maxime
+                        magnam
+                        recusandae voluptates quasi magni.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ml-5">
+        <div class="my-5 max-w-lg  border border-blue-400">
+            <h1 class="text-4xl font-semibold font bg-blue-400 text-white p-2">Usefull links</h1>
+            <ul class="p-2">
+                <li class="text-lg font-semibold text-blue-500"><a href="">Education Board Result</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Dc Office</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Dinajpur Education Board</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Directorate of Primary Education</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Ministry of Education</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Teachers Portal</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">National Curriculum and Texbook Board</a>
+                </li>
+            </ul>
+        </div>
+        <div class="my-5 max-w-lg border border-blue-400">
+            <h1 class="text-4xl font-semibold font bg-blue-400 text-white p-2">Explore</h1>
+            <ul class="p-2">
+                <li class="text-lg font-semibold text-blue-500"><a href="">Online Magazine 20182-19</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Use of MMC Monitoring App</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">IT Club</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Science Club</a></li>
+                <li class="text-lg font-semibold text-blue-500"><a href="">Photography Club</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
