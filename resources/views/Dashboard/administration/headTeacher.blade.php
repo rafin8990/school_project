0f1ba96978429e16993a0cd779f28180012d3d37
rafin8990 <rafin8990@gmail.com>
2024-01-23 18:07:16 +0600

    head teacher edited

--- resources/views/Dashboard/administration/headTeacher.blade.php
+++ resources/views/Dashboard/administration/headTeacher.blade.php
@@ -6,18 +6,30 @@
 <div>
     @foreach($principals as $principal)
     <div class=" ">
-        <div class="mx-3 my-5 ">
-            <div class="flex justify-center">
-            <img class=" rounded-lg my-5" src="{{ asset('assets/images/' . $principal->image) }}"
-                alt="Principal Image">
-            </div>
-            <h1 class=" text-4xl font-bold text-center">{{$principal->name}}</h1>
-            <h1 class=" text-xl text-center font-semibold">{{$principal->designation}}</h1>
-            <p class="text-center lg:mx-52">{{$principal->details}}</p>
-            <h1 class="text-xl text-center"><span class="font-bold">Address:</span> {{$principal->address}}</h1>
-            <h1 class="text-xl text-center "><span class="font-bold">Email:</span> {{$principal->email}}</h1>
-            <h1 class="text-xl text-center"><span class="font-bold">Phone Number:</span> {{$principal->phone_number}}</h1>
+        <div class="flex justify-end mx-2 my-2">
             <div class="flex justify-center"><a href="/updateHeadTeacher/{{$principal->id}}"><button class="btn btn-accent">Edit Head Teacher</button></a>
+            </div>
+        </div>
+        <div class=" flex">
+            <div class="md:w-1/2 mx-3 my-5">
+                <img class=" rounded-lg my-5" src="{{ asset('assets/images/' . $principal->image) }}"
+                    alt="Principal Image">
+                
+                
+
+                <h1 class="text-xl "><span class="font-bold">Address:</span> {{$principal->address}}</h1>
+                <h1 class="text-xl "><span class="font-bold">Email:</span> {{$principal->email}}</h1>
+                <h1 class="text-xl "><span class="font-bold">Phone Number:</span>
+                    {{$principal->phone_number}}
+                </h1>
+            </div>
+            <div class="md:w-1/2 mx-3 my-5">
+                <h1 class="text-3xl underline my-8">Head Teacher Welcome Speech</h1>
+                <p>{{$principal->details}}</p>
+                <h1 class=" text-2xl font-bold ">{{$principal->name}}</h1>
+                <p class=" text-xl font-semibold">Head Teacher</p>
+                <h1 class=" text-xl font-semibold">{{$principal->designation}}</h1>
+
             </div>
         </div>
     </div>
